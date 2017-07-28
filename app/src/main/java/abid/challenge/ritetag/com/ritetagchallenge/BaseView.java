9ef63fcc20237e2bcaec16a565298bcc1727cbfe
Abid Hasan <la4508@gmail.com>
2017-07-28 17:10:03 +0600

    set up

---
+++ app/src/main/java/abid/challenge/ritetag/com/ritetagchallenge/BaseView.java
@@ -0,0 +1,10 @@
+package abid.challenge.ritetag.com.ritetagchallenge;
+
+/**
+ * Created by VutkaBilai on 7/28/17.
+ * mail : la4508@gmail.com
+ */
+
+public interface BaseView<T> {
+  void setPresenter(T presenter);
+}
---
+++ app/src/main/java/abid/challenge/ritetag/com/ritetagchallenge/login/TwitterLogInActivity.java
@@ -0,0 +1,13 @@
+package abid.challenge.ritetag.com.ritetagchallenge.login;
+
+import abid.challenge.ritetag.com.ritetagchallenge.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterLogInActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_twitter_log_in);
+  }
+}
